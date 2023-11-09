d196adc2f566e9ee329b1137d40d58b2193252c1
Azar-Pav <exneps@gmail.com>
2023-11-09 17:44:28 +0300

    fix: один домент

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -8,7 +8,7 @@ export const ENDPOINTS = {
 }
 
 export const configMainApi = {
-  BASE_URL: 'https://api.movies-explorer.azar.nomoredomainsrocks.ru',
+  BASE_URL: 'movies-explorer.azar.nomoredomainsmonster.ru/api',
   HEADERS: {
     accept: 'application/json',
     'Content-type': 'application/json',
